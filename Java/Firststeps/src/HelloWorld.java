0e65e137b686fd280bed66aa0610ab335f875e40
NovaAce123 <98344753+NovaAce123@users.noreply.github.com>
2022-09-15 14:54:48 +0100

    Add files via upload

---
+++ Java/Firststeps/src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        number();
+    }
+    public static void number () {
+        int firstNumber;
+        firstNumber = 5;
+        System.out.println(firstNumber);
+    }
+    public static void data () {
+        double d = 4.5;
+        float f = 4.5f;
+        System.out.print();
+    }
+    public static void showoff () {
+
+    }
+}
