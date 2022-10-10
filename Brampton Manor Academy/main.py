99545bc367dfca8576962889aa30741fce5ce2ff
NovaAce123 <98344753+NovaAce123@users.noreply.github.com>
2022-10-10 12:04:28 +0100

    Update main.py

--- Brampton Manor Academy/main.py
+++ Brampton Manor Academy/main.py
@@ -3,11 +3,11 @@
 def length(card):
     x = 0
     while x == 0:
-        if len(card) == 16:
+        if len(str(card)) == 16:
             print("Valid card length")
             return 1
             x = 1
-        elif len(card) > 16:
+        elif len(str(card)) > 16:
             print("Too long")
             return 0
         else:
@@ -16,15 +16,28 @@ def length(card):
 
 
 def letter_check(card):
-    print(card.isalpha())
-    if (card).isalpha() == False:
-        print("Valid characters")
-        return 1
+    boolean = False
+    while boolean == False:
+        if str(card).isnumeric() == True:
+            print("Valid characters")
+            return 1
+        else:
+            print("Card number must be just numbers")
+            return 0
+
+def l_check(card):
+    card = str(card)
+    card = unicode(card, 'utf-8')
+    check = 1
+    x = card.isnumeric()
+    if x == check:
+        print("Valid")
     else:
-        print("Card number must be just numbers")
-        return 0
+        print("Incorrect")
+
 
 if __name__ == "__main__":
-    card = str(input("Enter Card Number: "))
-    length(card)
-    letter_check(card)
+    card = input("Enter Card Number: ")
+    #length(card)
+    #letter_check(card)
+    l_check(card)
